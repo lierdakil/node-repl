d6196b52c50325f4f870d16a1be14a62cf50b1d9
Nikolay Yakimov <root@livid.pp.ru>
2017-12-28 18:23:57 +0300

    Remove obsolete files

--- spec/node-repl-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import NodeReplView from '../lib/node-repl-view';
-
-describe('NodeReplView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
